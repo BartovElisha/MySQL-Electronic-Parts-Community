8a65885833fcf7d490ecdc845db998ec214fb91c
Elisha <alisher.b@gmail.com>
2022-06-18 21:37:15 +0300

    Added table companies

---
+++ Database scripts/components_community.sql
@@ -0,0 +1,15 @@
+-- CREATE DATABASE electronic_components_community_db;
+-- USE electronic_components_community_db;
+
+-- Create Database Tables
+-- CREATE TABLE companies
+-- (company_id INT NOT NULL AUTO_INCREMENT,
+-- company_name VARCHAR(45),
+-- company_description VARCHAR(45),
+-- logo_src VARCHAR(45),
+-- user_id INT,
+-- PRIMARY KEY (company_id));
+
+-- Show Database Tables
+-- SELECT * FROM companies;
+SELECT * FROM companies;
